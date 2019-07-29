ea2d312ba79f009211dfd99f2cfec0d02a9224f5
w.gao <williamgao520@gmail.com>
2019-07-28 22:02:57 -0700

    Add temporary player

--- src/NoaImpl.ts
+++ src/NoaImpl.ts
@@ -1,10 +1,11 @@
 import noaEngine = require("noa-engine");
+import pp = require("babylon-voxel-player");
 import 'babylonjs';
-import {MinecraftClient} from "@webpejs/network";
+import {EventType, MinecraftClient} from "@webpejs/network";
 
 export class NoaImpl {
 
-    private client: MinecraftClient;
+    private readonly client: MinecraftClient;
 
     private noaEngine;      // noaEngine
 
@@ -19,21 +20,27 @@ export class NoaImpl {
             debug: true,
             showFPS: true,
             silent: true,
-            chunkSize: 16,
-            chunkAddDistance: 8,
+            // chunkSize: 16,
+            // chunkAddDistance: 6,
+            chunkSize: 24,
+            chunkAddDistance: 3,
             // chunkRemoveDistance: 3,
             // blockTestDistance: 20,
             // texturePath: 'textures/',
-            playerStart: [128.5, 64, 128.5],        // change to spawn point
+            playerStart: [128.5, 65, 128.5],        // change to spawn point
+            // playerStart: [0, 4, 0],        // change to spawn point
             // playerHeight: 1.4,
             // playerWidth: 1.0,
+            playerHeight: 1.6,
             // playerAutoStep: true,
             // useAO: true,
             // AOmultipliers: [ 0.93, 0.8, 0.5 ],
             // reverseAOmultiplier: 1.0,
         };
 
+        // !! We should initialize noaEngine when PlayerSpawn event is fired
         this.noaEngine = noaEngine(opts);
+        this.noaEngine.setPaused(!this.client.hasSpawned);     // pause the game until we are spawned
 
         this.registerBlocks();
         this.registerListeners();
@@ -48,41 +55,104 @@ export class NoaImpl {
         const h = dat.height;
 
         const scene = noa.rendering.getScene();
-        const mesh = noa.BABYLON.Mesh.CreateBox('player', 1, scene);
-        mesh.scaling.x = mesh.scaling.z = w;
-        mesh.scaling.y = h;
+        // const mesh = noa.BABYLON.Mesh.CreateBox('player', 1, scene);
+        this.loadPlayer(scene, (mesh) => {
 
-        const offset = [0, h / 2, 0];
+            let player = pp({
+                scene: scene,
+                // Pass it the initial player color
+                player_color: new BABYLON.Color4(0.0274, 0.4313, 0.4392, 0.99),
+                // Pass it mesh height
+                player_height: 1.6,
+            });
 
-        noa.entities.addComponent(eid, noa.entities.names.mesh, {
-            mesh: mesh,
-            offset: offset
-        });
+            let player_mesh = player.get_player_mesh();
+            player_mesh.scaling.x = 0.45;
+            player_mesh.scaling.y = 0.45;
+            player_mesh.scaling.z = 0.45;
 
-        noa.inputs.down.on('fire', function () {
-            if (noa.targetedBlock) noa.setBlock(0, noa.targetedBlock.position)
-        });
+            // Add a player component to the player entity
+            noa.entities.addComponent(noa.playerEntity, noa.entities.names.mesh, {
+                mesh: player_mesh,
+                offset: [0, 1, 0],
+            });
 
-        noa.inputs.down.on('alt-fire', function () {
-            if (noa.targetedBlock) {
-                noa.addBlock(2, noa.targetedBlock.adjacent)
-            }
-        });
+            // Rotate player with camera
+            scene.registerBeforeRender(function () {
+                noa.entities.getMeshData(noa.playerEntity).mesh.rotation.y = noa.camera.heading;
+                player.update_particles();
+            });
 
-        noa.inputs.bind('alt-fire', 'E');
+            // Add Player movement animation
+            document.onkeyup = function (e) {
+                if (['87', '65', '83', '68', '37', '38', '39', '40'].indexOf(e.keyCode.toString()) > -1) {
+                    if (player.is_walking()) {
+                        player.stop_walking();
+                    }
+                }
+            };
+
+            document.onkeydown = function (e) {
+                if (['87', '65', '83', '68', '37', '38', '39', '40'].indexOf(e.keyCode.toString()) > -1) {
+                    if (!player.is_walking()) {
+                        player.start_walking();
+                    }
+                }
+            };
+
+            // mesh.scaling.x = mesh.scaling.z = w;
+            // mesh.scaling.y = h;
+
+            // console.log(mesh.material);
+            // (window as any).mat = mesh.material;
+
+            // const offset = [0, h - 0.5, 0];
+
+            // noa.entities.addComponent(eid, noa.entities.names.mesh, {
+            //     mesh: mesh,
+            //     // offset: offset
+            // });
+
+            noa.inputs.down.on('fire', function () {
+                if (noa.targetedBlock) noa.setBlock(0, noa.targetedBlock.position)
+            });
+
+            noa.inputs.down.on('alt-fire', function () {
+                if (noa.targetedBlock) {
+                    noa.addBlock(2, noa.targetedBlock.adjacent)
+                }
+            });
+
+            noa.inputs.bind('alt-fire', 'E');
 
 
-        let zoom = 0;
-        noa.on('tick', function (dt) {
-            const scroll = noa.inputs.state.scrolly;
-            if (scroll === 0) return;
+            let zoom = 0;
+            noa.on('tick', function (dt) {
+                const scroll = noa.inputs.state.scrolly;
+                if (scroll === 0) return;
 
-            zoom += (scroll > 0) ? 1 : -1;
-            if (zoom < 0) zoom = 0;
-            if (zoom > 10) zoom = 10;
-            noa.camera.zoomDistance = zoom;
-        });
+                zoom += (scroll > 0) ? 1 : -1;
+                if (zoom < 0) zoom = 0;
+                if (zoom > 10) zoom = 10;
+                noa.camera.zoomDistance = zoom;
+            });
 
+
+        })
+    }
+
+    private loadPlayer(scene, callback: (mesh) => void) {
+
+        callback(null);
+
+        // const mesh = this.noaEngine.BABYLON.Mesh.CreateBox('player', 1, scene);
+        // if (callback) callback(mesh);
+
+        // this.noaEngine.BABYLON.SceneLoader.ImportMesh("", "models/", "untitled.babylon", scene,
+        //     (newMeshes, particleSystems, skeletons) => {
+        //
+        //         if (callback) callback(newMeshes[0]);
+        //     });
     }
 
     private registerBlocks() {
@@ -90,36 +160,55 @@ export class NoaImpl {
         let textureURL = null;
         let brownish = [0.45, 0.36, 0.22];
         let greenish = [0.1, 0.8, 0.2];
-        let black = [0, 0, 0];
+        let black = [0.3, 0.3, 0.3];
+        let gray = [0.45, 0.5, 0.45];
         let white = [0.95, 0.95, 0.95];
         this.noaEngine.registry.registerMaterial('dirt', brownish, textureURL);
         this.noaEngine.registry.registerMaterial('grass', greenish, textureURL);
-        this.noaEngine.registry.registerMaterial('unknown', black, textureURL);
+        this.noaEngine.registry.registerMaterial('unknown', gray, textureURL);
+        this.noaEngine.registry.registerMaterial('bedrock', gray, textureURL);
         this.noaEngine.registry.registerMaterial('snow', white, textureURL);
 
         this.noaEngine.registry.registerBlock(1, {material: 'dirt'});
         this.noaEngine.registry.registerBlock(2, {material: 'grass'});
         this.noaEngine.registry.registerBlock(3, {material: 'unknown'});
-        this.noaEngine.registry.registerBlock(4, {material: 'snow'});
+        this.noaEngine.registry.registerBlock(4, {material: 'bedrock'});
+        this.noaEngine.registry.registerBlock(5, {material: 'snow'});
 
     }
 
     private registerListeners() {
 
+        this.client.on(EventType.PlayerSpawn, () => {
+
+            this.noaEngine.setPaused(false);
+        });
+
         let noa = this.noaEngine;
-        let world = this.client.world;
+        let client = this.client;
+        let map = {
+            // blockId: noaId
+            2: 2,       // grass
+            3: 1,       // dirt
+            7: 4,       // bedrock
+            80: 5,      // snow
+
+        };
 
         noa.world.on('worldDataNeeded', function (id, data, x, y, z) {
 
-            // todo: add callback when chunks are received and set data from there
+            let world = client.world;
+            let spawnX = world.worldInfo.spawn.x;       // use for flipping blocks on the x-axis
 
             for (let i = 0; i < data.shape[0]; ++i) {
                 for (let j = 0; j < data.shape[1]; ++j) {
                     for (let k = 0; k < data.shape[2]; ++k) {
 
+                        // if (y + j == 1) data.set(i, j, k, 2);
+
                         // flip the x-axis across the spawn point
                         // (2 * originX) - x
-                        let ax = 257 - (x + i);
+                        let ax = 2 * spawnX - (x + i);
 
                         // if (ax == 130 && (y + j) == 63 && (z + k) == 130) {
                         //     data.set(i, j, k, 3);
@@ -128,16 +217,11 @@ export class NoaImpl {
 
                         let blockId = world.getBlock(ax, y + j, z + k);
                         // It'd be cool if we use the same ID system
-                        if (blockId == 3) {     // dirt
-                            data.set(i, j, k, 1);       // brown
-                        } else if (blockId == 80) {     // snow
-                            data.set(i, j, k, 4);  // white-ish
-                        } else if (blockId != 0) {
-                            data.set(i, j, k, 2);  // green
-                        } else {
-                            // air
-                            data.set(i, j, k, 0);
-                        }
+
+                        let id = blockId == 0 ? 0 : map[blockId];
+                        if (id == undefined) id = 3;        // unknown
+
+                        data.set(i, j, k, id);
                     }
                 }
             }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
-import {MinecraftClient} from "@webpejs/network";
+import {EventType, LoginCredentials, MinecraftClient} from "@webpejs/network";
 import {NoaImpl} from "./NoaImpl";
 
 let client = new MinecraftClient(undefined, 19133);
+
+client.on(EventType.PlayerLoginRequest, (cred: LoginCredentials) => {
+    cred.displayName = 'WebNoaClient';
+});
 client.connect();
 
-client.onPlayerSpawn = () => {
+new NoaImpl(client).startEngine();
 
-    setTimeout(function () {
-        console.log('PlayerSpawn!');
-        new NoaImpl(client).startEngine();
-    }, 5000);
 
-};
+// client.on(EventType.PlayerSpawn, () => {
+//     setTimeout(function () {
+//         console.log('PlayerSpawn!');
+//         new NoaImpl(client).startEngine();
+//     }, 5000);
+//
+// });
+// new NoaImpl(null).startEngine();
 
 // setTimeout(startNoa.bind(this), 5000);
 // function startNoa() {}
